7d58db7a9e67f975cb31d6c187485bf4b7333dfe
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-11 16:35:26 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song 
+  
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@genres = [] 
+  @@artists = [] 
+  
+  def initialize(name, artist, genre)
+    @name
+    @artist
+    @genre
+    @@count += 1 
+    @@genres << genre
+    @@artists << artist 
+    
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.artists
+    
+  end
+  
+  def self.artist_count
+    
+  end
+  
+  def self.genre 
+  
+  end
+  
+  def self.genre_count
+    
+  end
+  
+end
