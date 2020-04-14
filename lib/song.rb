acc989e39c6c138c0ed40259a7400bc319de39e8
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-14 23:34:04 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song 
   
   attr_accessor :name, :artist, :genre 
@@ -7,9 +9,9 @@ class Song
   @@artists = [] 
   
   def initialize(name, artist, genre)
-    @name
-    @artist
-    @genre
+    @name = name 
+    @artist = artist
+    @genre = genre 
     @@count += 1 
     @@genres << genre
     @@artists << artist 
@@ -21,19 +23,37 @@ class Song
   end
   
   def self.artists
-    @@artists.uniq 
+    @@artists.uniq!
   end
   
   def self.artist_count
-    
+     artist_count = {}
+      @@artists.each do |artist|
+    if artist_count[artist] 
+      artist_count[artist] += 1 
+    else 
+      artist_count[artist] = 1 
+    end
+  end
+  artist_count
   end
   
-  def self.genre 
-  
+  def self.genres
+    @@genres.uniq!
   end
   
   def self.genre_count
-    
+    genre_count = {}
+      @@genres.each do |genre|
+    if genre_count[genre] 
+      genre_count[genre] += 1 
+    else 
+      genre_count[genre] = 1 
+    end
   end
+  genre_count
+end
   
 end
+    
+    
